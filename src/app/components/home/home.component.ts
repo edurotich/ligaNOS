313b236ce8111a830ac6ccff547be00ae6591b29
Miguel Ferreira <29511743+miguelsaferreira@users.noreply.github.com>
2017-10-24 16:48:09 +0100

    Now using flatMap on HomeComponent to handle multiple observable subscriptions

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Matches } from './matches.interface';
 import { DatePipe } from '@angular/common';
 import { FootdataService } from '../../services/footdata.service';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
   selector: 'app-home',
@@ -15,14 +16,40 @@ export class HomeComponent implements OnInit {
   matches: Matches[];
 
   constructor(private footdata: FootdataService) {
-    // this.currentMatchday = 9;
   }
 
   ngOnInit() {
     this.getLeagueInfoAndMatches();
+
+    // Another approach
+    // this.getLeagueInfoAndMatches2();
   }
 
+  /**
+   * Subscribes two observables from footdata service in order by using flatMap.
+   * Because getMatches is dependent from getLeagueInfo it will only make second
+   * request when first has finished
+   *
+   * @memberof HomeComponent
+   */
   getLeagueInfoAndMatches() {
+    this.footdata.getLeagueInfo().flatMap(data => {
+      this.currentMatchday = data.currentMatchday;
+      this.numberOfMatchdays = data.numberOfMatchdays;
+
+      return this.footdata.getMatches(this.currentMatchday);
+    }).subscribe(data => {
+      this.matches = data;
+    });
+  }
+
+  /**
+   * This approach only uses the subscribe method.
+   * Calls the second observable(getMatches) when getLeagueInfo is complete.
+   *
+   * @memberof HomeComponent
+   */
+  getLeagueInfoAndMatches2() {
     this.footdata.getLeagueInfo().subscribe(
       data => {
         this.currentMatchday = data.currentMatchday;
@@ -30,21 +57,24 @@ export class HomeComponent implements OnInit {
       }, (err: any) => console.log(err),
       () => {
         console.log('finished getLeagueInfo');
-        this.getMatches(this.currentMatchday); // calls getAllMatches when finished
+        this.footdata.getMatches(this.currentMatchday).subscribe(
+          data => {
+            console.log(data);
+            this.matches = data;
+          }, (err: any) => console.log(err),
+          () => { console.log('finished getAllMatches'); }
+        );
       }
     );
   }
 
-  getMatches(matchday: number) {
-    this.footdata.getMatches(matchday).subscribe(
-      d => {
-        console.log(d);
-        this.matches = d;
-      }, (err: any) => console.log(err),
-      () => { console.log('finished getAllMatches'); }
-    );
-  }
-
+  /**
+   * Creates a range from 1 to numberOfMatchdays that pushes
+   * the index to the array so it can be used inside *ngFor
+   *
+   * @returns array of items
+   * @memberof HomeComponent
+   */
   createRange() {
     const items = [];
     for (let i = 1; i <= this.numberOfMatchdays; i++) {
